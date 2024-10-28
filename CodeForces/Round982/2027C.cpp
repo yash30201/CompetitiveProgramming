f69c001aa5430329b09b37f25c9292389c21a436
yash30201 <54198301+yash30201@users.noreply.github.com>
2024-10-28 19:51:50 +0530

    Add CF problems

---
+++ CodeForces/Round982/2027C.cpp
@@ -0,0 +1,267 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#pragma GCC optimize("O3")
+
+using namespace std;
+using namespace __gnu_pbds;
+
+// find_by_order -> kth largest element (0 based)
+// order_of_key -> number of iterms strictly smaller than key (0 based)
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T> using v = vector<T>;
+template <typename T> using vv = vector<vector<T>>;
+template <typename T> using vvv = vector<vector<vector<T>>>;
+template <typename P, typename Q> using pp = pair<P, Q>;
+using vi = vector<int>;
+using vll = vector<long long>;
+using vb = vector<bool>;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+using vpii = vector<pair<int, int>>;
+using vpll = vector<pair<long long, long long>>;
+
+template <typename T>
+using pq = priority_queue<T>;
+
+template <typename T>
+using pqr = priority_queue<T, vector<T>, greater<T>>;
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+
+    size_t operator()(pair<uint64_t, uint64_t> x) const {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x.first + FIXED_RANDOM) ^
+               (splitmix64(x.second + FIXED_RANDOM) >> 1);
+    }
+};
+
+// template<typename P, typename T>
+// using ump = gp_hash_table<P, T, custom_hash>;
+template <typename P, typename T>
+using ump = unordered_map<P, T, custom_hash>;
+
+template <typename T>
+using us = unordered_set<T, custom_hash>;
+
+using ll = long long;
+const int INF = int(1e9);
+const ll INFL = ll(1e18);
+const int mod = 1000000007;
+using ull = unsigned long long;
+using ld = long double;
+
+int n_ones(int x) { return __builtin_popcount(x); }
+int n_ones(ll x) { return __builtin_popcountll(x); }
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define FORD(i, a, b) for (int i = b - 1; i >= a; i--)
+#define FORE(x, a) for (auto& x : a)
+#define all(a) a.begin(), a.end()
+#define mms(a, x) memset(a, x, sizeof(a));
+#define sz(a) a.size()
+#define pub push_back
+#define fi first
+#define se second
+#define endl '\n'
+#define PrintFixed(t, x) cout << fixed << setprecision(x) << t << endl;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+template <typename P, typename Q>
+ostream& operator<<(ostream& out, const pair<P, Q>& a) {
+    return out << "(" << a.fi << ", " << a.se << ")\n";
+}
+
+template <typename T>
+ostream& operator<<(ostream& out, const vector<T>& a) {
+    for (int i = 0; i < a.size(); i++) {
+        if (i) out << ' ';
+        out << a[i];
+    }
+    return out;
+}
+
+template <typename T, std::size_t S>
+ostream& operator<<(ostream& out, const array<T, S>& a) {
+    for (int i = 0; i < S; i++) {
+        if (i) out << ' ';
+        out << a[i];
+    }
+    return out;
+}
+
+template <typename P, typename Q>
+ostream& operator<<(ostream& out, const map<P, Q>& a) {
+    out << "\n[\n";
+    for (auto& [key, val] : a) out << "  " << key << " => " << val << endl;
+    return out << "]\n";
+}
+
+
+template <typename P, typename Q>
+ostream& operator<<(ostream& out, const ump<P, Q>& a) {
+    out << "\n[\n";
+    for (auto& [key, val] : a) out << "  " << key << " => " << val << endl;
+    return out << "]\n";
+}
+
+template <typename T>
+ostream& operator<<(ostream& out, const set<T>& a) {
+    out << "[";
+    for (const T& i : a) out << i << ", ";
+    return out << "]\n";
+}
+
+template <typename T>
+ostream& operator<<(ostream& out, const us<T>& a) {
+    out << "[";
+    for (const T& i : a) out << i << ", ";
+    return out << "]\n";
+}
+
+template <typename P, typename Q>
+istream& operator>>(istream& in, pair<P, Q>& a) {
+    return in >> a.fi >> a.se;
+}
+
+template <typename T>
+istream& operator>>(istream& in, vector<T>& a) {
+    for (T& i : a) in >> i;
+    return in;
+}
+
+template <typename T, size_t S>
+istream& operator>>(istream& in, array<T, S>& a) {
+    for (T& i : a) in >> i;
+    return in;
+}
+
+inline void op() { cout << endl; }
+template <typename P, typename... Q>
+inline void op(const P& a, Q... b) {
+    cout << a;
+    if constexpr (sizeof...(Q) == 0) {
+        cout << endl;
+    } else {
+        cout << ' ';
+        op(b...);
+    }
+}
+
+inline void ip() {}
+template <typename T, typename... Args>
+inline void ip(T& a, Args&... b) {
+    cin >> a;
+    ip(b...);
+}
+
+void err(istream_iterator<string> it) {}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+    cerr << ' ' << *it << " = " << a;
+    err(++it, args...);
+}
+
+
+#ifdef DEBUG
+#define see(args...)                             \
+    {                                            \
+        cerr << "LINE " << __LINE__;             \
+        string _s = #args;                       \
+        replace(_s.begin(), _s.end(), ',', ' '); \
+        stringstream _ss(_s);                    \
+        istream_iterator<string> _it(_ss);       \
+        err(_it, args);                          \
+        cerr << endl;                            \
+    }
+#else
+#define see(args...)
+#endif
+
+template <typename T> T maxi(const vector<T>& a) { return *max_element(all(a)); }
+template <typename T> T maxi(const T& a) { return a; }
+template <typename P, typename... T>
+P maxi(const P& a, T&&... b) {
+    P x = static_cast<P>(maxi(b...));
+    return max(a, x);
+}
+
+template <typename T> T mini(const vector<T>& a) { return *min_element(all(a)); }
+template <typename T> T mini(const T& a) { return a; }
+template <typename P, typename... T>
+P mini(const P& a, T&&... b) {
+    P x = static_cast<P>(mini(b...));
+    return min(a, x);
+}
+
+int px[] = {-1, 0, 1, 0};
+int py[] = {0, -1, 0, 1};
+string path_trace_dir = "DRUL";
+
+
+
+void solve() {
+    // Let's begin
+    int n;
+    ll res = 0;
+    ip(n);
+    v<ll> a(n);
+    ip(a);
+    v<pair<ll, int>> req;
+    ll exp = n;
+    FOR(i, 0, n) {
+        if (a[i] >= exp) {
+            req.pub({a[i] - exp, i});
+        }
+        exp--;
+    }
+    sort(all(req));
+    // see(req);
+    int m = req.size();
+    ump<ll, ll> dp;
+    dp[0] = n;
+    FOR(i, 0, m) {
+        if (dp.count(req[i].fi) == 0) continue;
+        ll x = req[i].fi + req[i].se;
+        if (dp.count(x)) dp[x] = max(dp[x], dp[req[i].fi] + req[i].se);
+        else dp[x] = dp[req[i].fi] + req[i].se;
+    }
+    for (auto [key, val] : dp) res = max(res, val);
+    op(res);
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
+
+
+
+
+
+
+
+
