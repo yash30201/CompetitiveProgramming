9e819a1499206163ea59060e099ac62f608736a2
yash30201 <54198301+yash30201@users.noreply.github.com>
2025-01-18 17:24:27 +0530

    Simplyfy fenwick range add point query

--- CodeSnippets/Trees/FenwickRangeUpdatePointQuery.cpp
+++ CodeSnippets/Trees/FenwickRangeUpdatePointQuery.cpp
@@ -5,17 +5,17 @@ using namespace std;
 struct FenwickTree {
     int n;
     vector<int> t;
-    FenwickTree(int nn): n(nn) {t.assign(n + 1, 0);}
-    void add(int i, int val) {
-        for ( ; i <= n ; i += (i & (-i))) t[i] += val;
+    FenwickTree(int nn): n(nn) {t.assign(n, 0);}
+    void update(int i, int val) {
+        for ( ; i < n ; i = (i | (i + 1))) t[i] += val;
     }
-    void add(int l, int r, int val) { // [l, r]
-        add(l, val);
-        add(r + 1, -val);
+    void update(int l, int r, int val) { // [l, r]
+        update(l, val);
+        update(r + 1, -val);
     }
-    int get(int i) {
+    int query(int i) {
         int result = 0;
-        for ( ; i > 0 ; i -= (i & (-i))) result += t[i];
+        for ( ; i >= 0 ; i = (i & (i + 1)) - 1) result += t[i];
         return result;
     }
 };
@@ -31,12 +31,12 @@ int main() {
     vector<int> a{1, 2, 3, 4, 5, 6};
     // 1 indexed based tree
     FenwickTree tree((int)a.size());
-    tree.add(1, 4, -1);
-    assert(tree.get(2) + a[1] == 1);
-    assert(tree.get(5) + a[4] == 5);
-    tree.add(2, 5, 10);
-    assert(tree.get(5) + a[4] == 15);
-    assert(tree.get(2) + a[1] == 11);
+    tree.update(0, 3, -1);
+    assert(tree.query(1) + a[1] == 1);
+    assert(tree.query(4) + a[4] == 5);
+    tree.update(1, 4, 10);
+    assert(tree.query(4) + a[4] == 15);
+    assert(tree.query(1) + a[1] == 11);
     cout << "Test Passed!\n";
     return 0;
 }
